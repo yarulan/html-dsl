23a77cef487f0138cb83a3ee167f130afd8ffca7
Yaroslav Ulanovych <yar@ulan.pro>
2017-04-24 13:19:57 +0300

    Cosmetics

--- core/test/htmldsl/DslTest.scala
+++ core/test/htmldsl/DslTest.scala
@@ -11,31 +11,38 @@ abstract class DslTest extends FreeSpec {
   val expectedHtml: String
 
   "Dsl should" - {
-    "provide nice syntax for nodes without attributes" in {
-      val e = div {
-        span {
-          text("Hello")
-        }
-        span {
-          text("World")
-        }
+    "provide nice syntax for" - {
+      "single nodes" in {
+        val e = div()
+        check(e, """<div></div>""")
       }
-      check(e, """<div><span>Hello</span><span>World</span></div>""")
-    }
 
-    "provide nice syntax for nodes with attributes" in {
-      val f = form(`class` := "form-horizontal", {
-        div(`class` := "form-group", {
-          val inputId = "email"
-          label(`class` := "control-label col-sm-2", `for` := inputId, {
-            text("Email")
-          })
-          div(`class` := "col-sm-10", {
-            input(`type` := "email", `class` := "form-control", id := inputId, placeholder := "Enter email")
+      "nodes without attributes" in {
+        val e = div {
+          span {
+            text("Hello")
+          }
+          span {
+            text("World")
+          }
+        }
+        check(e, """<div><span>Hello</span><span>World</span></div>""")
+      }
+
+      "nodes with attributes" in {
+        val f = form(`class` := "form-horizontal", {
+          div(`class` := "form-group", {
+            val inputId = "email"
+            label(`class` := "control-label col-sm-2", `for` := inputId, {
+              text("Email")
+            })
+            div(`class` := "col-sm-10", {
+              input(`type` := "email", `class` := "form-control", id := inputId, placeholder := "Enter email")
+            })
           })
         })
-      })
-      check(f, expectedHtml)
+        check(f, expectedHtml)
+      }
     }
 
     "allow to use symbols as tag names" in {
